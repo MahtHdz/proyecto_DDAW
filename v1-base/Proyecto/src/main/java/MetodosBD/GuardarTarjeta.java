92fe37927dd506db4252c879b0addb2a50c65f5b
Maht <maht_hernandez@outlook.com>
2021-04-30 16:47:19 -0500

    nuevas conexiones en la bd

--- v1-base/Proyecto/src/main/java/MetodosBD/GuardarTarjeta.java
+++ v1-base/Proyecto/src/main/java/MetodosBD/GuardarTarjeta.java
@@ -5,6 +5,11 @@
  */
 package MetodosBD;
 
+import Pojos.TablaTCRC;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author maht_
@@ -13,7 +18,28 @@ public class GuardarTarjeta {
     
     public GuardarTarjeta(){}
     
-    public void sendCard(){
-        
+    public void sendCard(String clase, String listaSuper, String listaSub, TablaTCRC tabla[]){
+            int id = 0;
+            DB DBOps = new DB();
+            DBOps.openConn();
+            Statement SQLQueryOp = DBOps.getSQLQueryOp();
+            String SQL_discoveringID = "SELECT id FROM historia_usuario";
+            String SQL = "INSERT INTO tarjeta ('clase', 'lista_super', 'lista_sub')"
+                    + " VALUES ('"+ clase +"', '"+ listaSuper +"', '"+ listaSub +"'";
+            ResultSet query = DBOps.getSQLQuery(SQLQueryOp, SQL_discoveringID);
+            try{
+                if(query != null){
+                    query.last();
+                    id = query.getInt("id");
+                    id += 1;
+                }
+                SQLQueryOp.execute(SQL);
+                for(int i = 0; i < tabla.length; i++)
+                    new GuardarTablaTCRC(id, tabla[i].getResponsabilidad(), tabla[i].getColaborador());
+                
+                DBOps.closeConn();
+            }catch(SQLException e){
+                System.out.println(e.getMessage());
+            }   
     }
 }
